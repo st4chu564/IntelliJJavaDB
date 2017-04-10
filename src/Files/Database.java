7c869089f19506c4d4dccc2e8b6733eb382e228b
Stachu <stasiek94@gmail.com>
2017-04-10 13:29:05 +0200

    Bit more work on opening main file, created .dbm extension and worked on exceptions

--- src/Files/Database.java
+++ src/Files/Database.java
@@ -1,6 +1,7 @@
 package Files;
 
 import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,38 +10,62 @@ public class Database {
     private String fileName;
     private BufferedReader read = null;
     private List<String> list = new ArrayList<>();
-    public Database(String fileName){
-        this.fileName = fileName;
-        try {
-            File file = new File(fileName);
-            read = new BufferedReader(new FileReader(file));
-            String text;
-            while ((text = read.readLine()) != null) {
-                list.add(text);
-            }
-        }
-        catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        finally{
+    public Database(String fileName) {
+        if (fileName == "")
+            System.out.println("First, create a file");
+        else {
+            this.fileName = fileName;
             try {
-                if (read != null) {
-                    read.close();
+                File file = new File(this.fileName);
+                read = new BufferedReader(new FileReader(file));
+                String text;
+                while ((text = read.readLine()) != null) {
+                    list.add(text);
                 }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (read != null) {
+                        read.close();
+                    }
+                } catch (IOException e) {
+                }
             }
         }
     }
     public Database(){
         this.fileName = "";
     }
-    public void printContent(){
-        for(String value: list){
-            System.out.println(value);
+    public int createNew(String fileName){
+        this.fileName = fileName;
+
+        try {
+            File out = new File(this.fileName);
+            if(out.exists()){
+                System.out.println("File already exists");
+                this.fileName = "";
+                return 1;
+            }
+            out.createNewFile();
         }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        /*finally{
+
+        }*/
+        return 0;
+    }
+    public void printContent(){
+        if(list.isEmpty())
+            System.out.println("List is empty");
+        else
+            for(String value: list){
+            System.out.println(value);
+            }
     }
     public static void main (String []args){
 
--- src/Files/Main.java
+++ src/Files/Main.java
@@ -5,9 +5,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        System.out.print("Give file name: ");
-        String fileToOpen = read.next();
-        Database base = new Database(fileToOpen);
-        base.printContent();
-    }
+        int choice;
+        do {
+            System.out.println("1. Open existing file");
+            System.out.println("2. Create new file");
+            choice = read.nextInt();
+        }while(choice != 1 && choice != 2);
+        if(choice == 1) {
+            System.out.print("Give file name: ");
+            String fileToOpen = read.next();
+            Database base = new Database(fileToOpen);
+            base.printContent();
+        }
+        else if(choice == 2){
+            Database base = new Database();
+            String name;
+            int result;
+            do {
+                System.out.print("Enter file name: ");
+                 name = read.next();
+                if (name.toLowerCase().contains("."))
+                    name = name.substring(0, name.indexOf(".")) + ".dbm";
+                else
+                    name += ".dbm";
+                result = base.createNew(name);
+            }while(result != 0);
+            base.printContent();
+        }
+        }
 }
